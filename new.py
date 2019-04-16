4157c031bc90a4ef02751398b5bb6159319b4194
Deron Smith <smithderon42@yahoo.com>
2019-04-16 14:22:47 -0500

    Tuesday stuff

---
+++ new.py
@@ -0,0 +1,53 @@
+# greatest = "Are you not entertained!?"
+# print (greatest)
+
+# magic_coins = 25
+# found_coins = 24
+# found_coins += magic_coins
+# treasure = "I found %d magic coins." %(found_coins)
+# print (treasure)
+# the_greatest = "Deron"
+# age = 23
+# dream = "game design and, or, programming"
+# mine = "My name is %s, my age is %d, and %s is my mission!" %(the_greatest, age, dream)
+# print (mine)
+# point = "needs to stop"
+# valid = "let me help you understand why."
+# valid_point = "Dragonball {} masquerading as an anime. Confused are you? Let me {}".format(point, valid)
+# print (valid_point)
+# a = 29
+# b = 42 * 1
+# c = a + b
+# print (c)
+# number_of_the_gods = "9999"
+
+# mass_destruction = int(number_of_the_gods) + 90000
+# print (mass_destruction)
+# title = input("What's your name, hm? ")
+
+# call_me = "You can call me %s." %(title)
+
+# print (call_me)
+# age_current = input("How old are you? ")
+# current_year = 2019
+# birth_year = current_year - int(age_current) - 1
+# # print (birth_year)
+# if birth_year >= 2000:
+#     print ("So you need to find the exit...today, please!")
+# else:
+#     print ("Well, enjoy all the amenities of...the library")
+# destinations = 40
+# paths = 25
+# obstacles = 15
+# if destinations > paths:
+#     print("Well, we can always just go somewhere else.")
+# damage = 100
+# while (damage < 10000):
+#     damage += 11
+#     print ("You dealt" + str(damage) + "damage!")
+# print ("You little cheater...")
+trigger = ""
+while trigger != "pleb":
+    trigger = input("Go ahead, call me pleb one more time! ")
+    trigger.lower()
+print("Get sucker punched!")
